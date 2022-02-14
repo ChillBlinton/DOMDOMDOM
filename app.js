5a77f32c5e68c6151a34526501e41e155dade66d
CB <wtbatson@gmail.com>
2022-02-14 09:52:27 -0600

    added context

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 function Button() {
-    
+
+    //needs main container
+
     //create button
     let button = document.createElement('button');
     button.className = "btn btn-primary btn-md d-block shadow mx-auto my-4";
@@ -15,13 +17,15 @@ function Button() {
 
 
     //adding listener
-
     button.addEventListener('click', () => {
         clickCounter++;
         let square = document.createElement('div');
         square.className = 'square d-flex justify-content-center align-items-center mx-3 mb-3 shadow text-white';
         let squareText = document.createTextNode(clickCounter);
-        square.id = clickCounter;
+        square.id = clickCounter;  //needs add clickCounter
         section.appendChild(square);
+
+        //addEventListener(mouseover)
+        //addEventListener
     
-}
+};
